3c182ba2cc42907815fb20ce5eb62814ed7eee6c
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-08-12 09:12:20 +0100

    deployed

--- user_management/settings.py
+++ user_management/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-default-secret-key-for-dev')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
 
 
 ALLOWED_HOSTS = []
